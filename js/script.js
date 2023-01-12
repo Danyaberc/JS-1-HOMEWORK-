94ffeeda2271cd97d9738d567eca58056fa521cf
unknown <berimecdanil963@gmail.com>
2023-01-12 08:38:33 +0300

    myfisrstjs

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let zamena = document.querySelector('.project');
+console.log(zamena);
+
+zamena.addEventListener('mouseenter', () => {
+   zamena.innerHTML = 'NewTEXT'
+});
+
+zamena.addEventListener('mouseout', () => {
+   zamena.innerHTML = 'blablablchat'
+})
+
+
+
+
